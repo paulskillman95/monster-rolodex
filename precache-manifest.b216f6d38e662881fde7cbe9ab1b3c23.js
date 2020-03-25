73b5acedbb84ab8f884596ff1967138fd2403334
Paul Skillman <paulskillman95@gmail.com>
2020-03-25 12:53:31 -0600

    Updates

---
+++ precache-manifest.b216f6d38e662881fde7cbe9ab1b3c23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2cb6f6ffd8882721b199947fd4c3db",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "02c01e24e9b2fb3d1d46",
+    "url": "/monster-rolodex/static/css/main.48361f09.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02c01e24e9b2fb3d1d46",
+    "url": "/monster-rolodex/static/js/main.e2b4532c.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
